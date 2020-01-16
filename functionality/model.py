60385d0a2335008272ff085295d6af10dab82c2e
FrancescaSchiav <francesc@bu.edu>
2020-01-16 23:59:32 +0100

    added the nodes and edges graph in model.py

--- functionality/model.py
+++ functionality/model.py
@@ -1,6 +1,8 @@
 import random
 import numpy as np
 import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
 
 from mesa import Model
 from mesa.space import MultiGrid
@@ -14,6 +16,7 @@ class Friends(Model):
     '''
     Model of making friends in a spatial setting
     '''
+
     def __init__(
             self,
             height=20, width=20,
@@ -58,10 +61,42 @@ class Friends(Model):
         mat = pd.DataFrame(np.zeros((n, n)), index=ids, columns=ids)
         return mat
 
+    def store(self, Graph, bool):
+        if bool:
+            if list(Graph)[0]==0:
+                global M
+                M = Graph
+            else:
+                M = nx.compose(M,Graph)    
+        else:
+            #to plot the nodes and edges of friendships
+            scores = Graph.to_numpy()
+            for j in range(len(scores)):
+                for t in range(len(scores[0])):
+                    if scores[j][t]!=0:
+                        M.add_edge(j,t, weight = scores[j][t])
+        
+            close=[(u,v) for (u,v,d) in M.edges(data=True) if d['weight'] <0.3]
+            mid=[(u,v) for (u,v,d) in M.edges(data=True) if d['weight'] >0.3 and d['weight']<0.6]
+            far=[(u,v) for (u,v,d) in M.edges(data=True) if d['weight'] >=0.6]
+
+            nx.draw_networkx_nodes(M, nx.get_node_attributes(M, 'pos'), node_size=80, node_color='dimgrey')
+            nx.draw_networkx_edges(M, nx.get_node_attributes(M, 'pos'), edgelist=close, width=3.5, edge_color='navy')
+            nx.draw_networkx_edges(M, nx.get_node_attributes(M, 'pos'), edgelist=mid, width=2, edge_color='royalblue')
+            nx.draw_networkx_edges(M, nx.get_node_attributes(M, 'pos'), edgelist=far, width=0.8, edge_color='skyblue')
+
+            plt.show()
+
     def new_agent(self, pos):
-        agent = Human(self.next_id(), self, pos)
+        ID = self.next_id()
+        agent = Human(ID, self, pos)
         self.grid.place_agent(agent, pos)
         self.schedule.add(agent)
+
+        #ID and initial pos also used for node graph
+        M = nx.Graph()
+        M.add_node((ID-1), pos=pos)
+        self.store(M, True)
 
     def step(self):
         self.schedule.step()
@@ -74,3 +109,4 @@ class Friends(Model):
         for i in range(step_count):
             self.step()
         print(self.friends_score)
+        self.store(self.friends_score, False)  

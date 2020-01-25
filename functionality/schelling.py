59c264def7c2977c2f44fa22063e9989ce10ec1a
Herman Aadamsoo <h.aadamsoo@amenti.ch>
2020-01-25 12:51:09 +0100

    Schelling improvements: :wrench:
    * agents move only to better cells in schelling model
    * server visualization created for schelling model

--- functionality/schelling.py
+++ functionality/schelling.py
@@ -10,22 +10,33 @@ class SchellingAgent(Agent):
         super().__init__(pos, model)
         self.pos = pos
         self.character = np.random.random()
+        self.did_move = True
 
     def step(self):
-        distance = 0
-        neighbors = self.model.grid.neighbor_iter(self.pos)
-        for neighbor in neighbors:
-            distance += np.abs(neighbor.character - self.character)
-        avg_distance = distance / 8
-        if avg_distance > self.model.tolerance:
-            self.model.grid.move_to_empty(self)
+        self.did_move = False
+        cur_distance = self.get_cell_fit(self.pos, self.character)
+        if cur_distance > self.model.tolerance:
+            for pos in self.model.grid.empties:
+                cell_dist = self.get_cell_fit(pos, self.character)
+                if cell_dist < cur_distance:
+                    self.model.grid.move_agent(self, pos)
+                    self.did_move = True
+                    break
         else:
             self.model.happy += 1
+
+    def get_cell_fit(self, pos, character):
+        distance = 0
+        neighbors = self.model.grid.neighbor_iter(pos)
+        for neighbor in neighbors:
+            distance += np.abs(neighbor.character - character)
+        avg_distance = distance / 8
+        return avg_distance
 
 
 class SchellingModel(Model):
 
-    def __init__(self, height, width, tolerance, population):
+    def __init__(self, height=20, width=20, tolerance=0.3, population=200):
         self.height = height
         self.width = width
         self.tolerance = tolerance
@@ -50,8 +61,20 @@ class SchellingModel(Model):
             self.grid.position_agent(agent, (x, y))
             self.schedule.add(agent)
 
+    def happy_ratio(self):
+        agents = self.schedule.agents
+        return self.happy / len(agents)
+
     def step(self):
         self.happy = 0  # Reset counter of happy agents
         self.schedule.step()
-        if self.happy == self.schedule.get_agent_count():
+        if self.happy_ratio() > 0.99:
             self.running = False
+
+
+if __name__ == '__main__':
+    md = SchellingModel(20, 20, 0.3, 200)
+    for i in range(200):
+        md.step()
+    print(i)
+    print(md.happy)
